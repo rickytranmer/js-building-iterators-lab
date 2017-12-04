54eec9b4128e9a0336d2a9f2cd8a04aa8fd4a6e8
rickytranmer <rwtranmer@gmail.com>
2017-12-04 16:23:51 -0700

    adds myMap.js functionality

--- myMap.js
+++ myMap.js
@@ -1,9 +1,13 @@
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Map
 
 function myMap(arr, callback) {
+	var arr2 = [];
 
-//  CODE INSIDE HERE   //
-
+	for (let i=0; i<arr.length; i++) {
+	  arr2[i] = callback(arr[i], i, arr);
+	}
+	
+	return arr2;
 }
 
 /*
